1ff3d2decfc1b78c29d1a5b683393b99c8245056
Aashish Sheshadri <aashish.sheshadri@gmail.com>
2013-11-13 01:15:10 -0600

    printing p-values on statistical significance

--- processingScripts/statisticalSig/testHypothesis.m
+++ processingScripts/statisticalSig/testHypothesis.m
@@ -2,10 +2,10 @@ function [boolResultMetA, boolResultMetF] = testHypothesis(actualMetA,samplesMet
     sigThresh = 0.05;
     compAbsSamples = abs(samplesMetA - samplesMetB);
     compAbs = abs(actualMetA - actualMetB);
-    
     metAP = sum(compAbsSamples(:,1)>compAbs(1))/size(compAbsSamples,1);
-    metFP = sum(compAbsSamples(:,2)>compAbs(2))/size(compAbsSamples,2);
-    
+    metFP = sum(compAbsSamples(:,2)>compAbs(2))/size(compAbsSamples,1);
+    disp(['Two sided p value Accuracy: ' num2str(metAP)]);
+    disp(['Two sided p value FMeasure: ' num2str(metFP)]);
     boolResultMetA = metAP <= sigThresh;
     boolResultMetF = metFP <= sigThresh;    
 end
